a7428ec8df00b01d8dda359dd21171cf792cc7b9
Cercva1 <giorgi.tsertsvadze@sa.stud.vu.lt>
2025-05-22 20:12:30 +0300

    Updating Users file with class and separet functions in order to have registartion and login system

---
+++ classes/User.php
@@ -0,0 +1,50 @@
+<?php 
+
+class User {
+    private $conn;
+    public $id;
+    public $username;
+    public $password;
+    public $aes_key_encrypted;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function register() {
+        $query = "INSERT INTO users (username, password_hash, aes_key_encrypted) VALUES (:username, :password_hash, :aes_key_encrypted)";
+        $stmt = $this->conn->prepare($query);
+
+        $password_hash = password_hash($this->password, PASSWORD_DEFAULT); // hash the password
+
+        $key = openssl_random_pseudo_bytes(32); // generate 256-bit AES key
+        $iv = openssl_random_pseudo_bytes(16); // generate IV for encryption
+        $encrypted_key = openssl_encrypt($key, 'aes-256-cbc', $this->password, 0, $iv); // encrypt key using user password
+
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":password_hash", $password_hash);
+        $stmt->bindParam(":aes_key_encrypted", $encrypted_key . ':' . base64_encode($iv)); // store key and IV together
+
+        return $stmt->execute();
+    }
+
+    public function login() {
+        $query = "SELECT * FROM users WHERE username = :username";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":username", $this->username);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            if (password_verify($this->password, $row['password_hash'])) { // verify user password
+                $this->id = $row['id'];
+                $this->aes_key_encrypted = $row['aes_key_encrypted'];
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+?>
